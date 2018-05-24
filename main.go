7e9f95dceb61c7d1692cefd59dd0e0d839a27b31
Kai Hendry <kai.hendry@unee-t.com>
2018-05-24 10:43:33 +0800

    Assuming arn:aws:sns:ap-southeast-1:812644853088:atest:25cf89fb-7955-4b32-a48d-ecdc3560ace6 is setup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -8,11 +9,18 @@ import (
 	"os"
 
 	"github.com/apex/log"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
 	"gopkg.in/antage/eventsource.v1"
 )
 
 type handler struct {
 	es eventsource.EventSource
+}
+
+type sqsmessage struct {
+	Message string
 }
 
 func main() {
@@ -30,12 +38,43 @@ func main() {
 	}
 }
 
+func (h handler) receiveSQS() {
+
+	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("uneet-dev"))
+	if err != nil {
+		log.WithError(err).Error("failed to load config")
+		return
+	}
+
+	// https://godoc.org/github.com/aws/aws-sdk-go-v2/service/sqs#ReceiveMessageRequest
+	sqssvc := sqs.New(cfg)
+
+	msgReq := sqssvc.ReceiveMessageRequest(&sqs.ReceiveMessageInput{
+		QueueUrl: aws.String("https://sqs.ap-southeast-1.amazonaws.com/812644853088/atest"),
+	})
+
+	log.Info("Requesting from SQS")
+	msgs, err := msgReq.Send()
+	if err != nil {
+		panic(err)
+	}
+	var sqsm sqsmessage
+	for _, msg := range msgs.Messages {
+		log.Infof("SQS Receive Message %s", &msg.Body)
+		json.Unmarshal([]byte(*msg.Body), &sqsm)
+		log.Infof("here %s", sqsm.Message)
+	}
+
+	h.es.SendEventMessage(sqsm.Message, "", "")
+}
+
 func (h handler) hook(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		fmt.Fprintln(w, err.Error())
 	}
 	h.es.SendEventMessage(string(dump), "", "")
+	h.receiveSQS()
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
